414832f86c936708573ebd553a9e697115362ff3
zby-66 <1753058819@qq.com>
2020-03-20 11:32:28 +0800

    TopContainer

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,175 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <TopContainer></TopContainer>
+    <!-- <BHeader></BHeader>
+    <BContent :rows="rows"></BContent>
+    <BNavSide :options="options" v-on:change="isShowMask"></BNavSide> -->
+    <div class="wnd-mask" ref="mask" v-show="showMask"></div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import TopContainer from './components/common/TopContainer.vue'
+// import BHeader from 'components/common/BHeader.vue'
+// import BContent from 'components/content/BContent.vue'
+// import BNavSide from 'components/nav/BNavSide'
 
-#nav {
-  padding: 30px;
+// import { mapGetters } from 'vuex'
+export default {
+  name: 'app',
+  components: {
+    TopContainer,
+    // BHeader,
+    // BContent,
+    // BNavSide
+  },
+  mounted() {
+    // this.$store.dispatch('getContentRows')
+  },
+  data() {
+    return {
+      showMask: false
+    }
+  },
+  watch: {
+    options() {
+      console.log('options 变化了')
+    },
+    items() {
+      console.log('items 变化了')
+    }
+  },
+  computed: {
+    // ...mapGetters([
+    //   'requesting',
+    //   'error',
+    //   'rows'
+    // ]),
+    options() {
+      let options = {
+        offset: 100, //偏移的距离
+        items: this.rows,
+        offsetTop: 0 //距离顶部距离
+      }
+      return options
+    }
+  },
+  methods: {
+    isShowMask() {
+      this.showMask = !this.showMask
+    }
+  }
 }
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style lang="stylus">
+  #app 
+    font-family "Microsoft YaHei",Arial,Helvetica,sans-serif
+    -webkit-font-smoothing antialiased
+    font-size 12px
+    margin 0
+    padding 0
+    background #fff
+    color #222
+    min-width 990px
+    tap-highlight-color transparent
+    -webkit-tap-highlight-color transparent
+    .wnd-mask
+      position fixed
+      width 100%
+      height 150%
+      background-color #000
+      opacity .5!important
+      z-index 1000
+      top 0px
+      left 0px
+      transition .2s
+//   html, body, div, span, applet, object, iframe,
+// h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+// a, abbr, acronym, address, big, cite, code,
+// del, dfn, em, img, ins, kbd, q, s, samp,
+// small, strike, strong, sub, sup, tt, var,
+// b, u, i, center,
+// dl, dt, dd, ol, ul, li,
+// fieldset, form, label, legend,
+// table, caption, tbody, tfoot, thead, tr, th, td,
+// article, aside, canvas, details, embed,
+// figure, figcaption, footer, header,
+// menu, nav, output, ruby, section, summary,
+// time, mark, audio, video, input {
+//     margin: 0;
+//     padding: 0;
+//     border: 0;
+//     font-size: 100%;
+//     font-weight: normal;
+//     vertical-align: baseline;
+// }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+// /* HTML5 display-role reset for older browsers */
+// article, aside, details, figcaption, figure,
+// footer, header, menu, nav, section {
+//     display: block;
+// }
+
+// body {
+//     line-height: 1;
+// }
+
+// blockquote, q {
+//     quotes: none;
+// }
+
+// blockquote:before, blockquote:after,
+// q:before, q:after {
+//     content: none;
+// }
+
+// table {
+//     border-collapse: collapse;
+//     border-spacing: 0;
+// }
+
+// /* custom */
+// a {
+//     color: #7e8c8d;
+//     text-decoration: none;
+//     -webkit-backface-visibility: hidden;
+//     cursor: pointer;
+// }
+
+// li {
+//     list-style: none;
+// }
+
+// /*::-webkit-scrollbar {
+//     width: 16px;
+//     height: 5px;
+// }
+
+// ::-webkit-scrollbar-track-piece {
+//     background-color: rgba(0, 0, 0, 0.2);
+//     -webkit-border-radius: 6px;
+// }
+
+// ::-webkit-scrollbar-thumb:vertical {
+//     height: 5px;
+//     background-color: rgba(125, 125, 125, 0.7);
+//     -webkit-border-radius: 6px;
+// }
+
+// ::-webkit-scrollbar-thumb:horizontal {
+//     width: 5px;
+//     background-color: rgba(125, 125, 125, 0.7);
+//     -webkit-border-radius: 6px;
+// }*/
+
+// html, body {
+//     width: 100%;
+// }
+
+// body {
+//     -webkit-text-size-adjust: none;
+//     -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+// }
+
 </style>
---
+++ src/components/common/TopContainer.vue
@@ -0,0 +1,325 @@
+<template>
+  <div class="top_container">
+    <div class="z_top b-header-blur b-header-blur-black">
+      <div class="b-header-mask-wrp">
+        <div
+          class="b-header-mask-bg"
+          style="background-image: url(//i0.hdslb.com/bfs/archive/ed92db305ae43c7fc8a59b1789934caa2636b876.png);"
+        ></div>
+        <div class="b-header-mask"></div>
+      </div>
+      <div class="z_header">
+        <div class="z_top_nav">
+          <ul>
+            <li class="home">
+              <a class="i-link" href="//www.bilibili.com/">
+                <span>主站</span>
+              </a>
+            </li>
+            <li class="hbili">
+              <a class="i-link" href="//h.bilibili.com/" title="画友">画友</a>
+            </li>
+            <li class="b-gc" hasframe="true">
+              <a class="i-link" href="//game.bilibili.com/" title="游戏中心">游戏中心</a>
+            </li>
+            <li class="live" hasframe="true">
+              <a class="i-link" target="_blank" href="//live.bilibili.com/" title="直播">直播</a>
+            </li>
+            <li class="manga" hasframe="true">
+              <a class="i-link" target="_blank" href="//manga.bilibili.com/" title="漫画">漫画</a>
+            </li>
+            <li class="b-zb">
+              <a class="i-link" target="_blank" href="//zb.bilibili.com/" title="周边">周边</a>
+            </li>
+            <li class="planet">
+              <a class="i-link" target="_blank" href="//planet2017.bilibili.com/" title="拜年祭2019">
+                拜年祭
+                <em class="new"></em>
+              </a>
+            </li>
+            <li></li>
+          </ul>
+        </div>
+        <div class="uns_box">
+          <ul class="menu">
+            <li id="i_menu_login_reg" guest="yes" class="u-i" style="display: list-item">
+              <a
+                id="i_menu_login_btn"
+                class="i-link login"
+                href="https://account.bilibili.com/login"
+              >
+                <span>登录</span>
+              </a>
+              <i class="s-line"></i>
+              <a
+                id="i_menu_register_btn"
+                class="i-link reg"
+                href="https://www.bilibili.com/register"
+              >
+                <span>注册</span>
+              </a>
+            </li>
+            <li class="u-i b-post">
+              <a
+                class="i-link"
+                href="http://member.bilibili.com/v/video/submit.html"
+                target="_blank"
+                @mouseenter="isShowPostMenu = !isShowPostMenu"
+                @mouseleave="isShowPostMenu = !isShowPostMenu"
+              >投稿</a>
+              <PostMaterial v-show="isShowPostMenu"></PostMaterial>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostMaterial from "./PostMaterial.vue";
+export default {
+  data() {
+    return {
+      isShowPostMenu: false
+    };
+  },
+  components: {
+    PostMaterial
+  },
+  methods: {
+    showPostMenu() {
+      this.isShowPostMenu = !this.isShowPostMenu;
+    }
+  }
+};
+</script>
+
+<style lang="stylus">
+.top_container {
+  color: #222;
+  position: relative;
+  z-index: 10000;
+
+  .z_top {
+    background-color: transparent;
+    height: 42px;
+    background: #fff;
+    box-shadow: rgba(0, 0, 0, 0.1) 0 1px 2px;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    position: relative;
+    z-index: 10000;
+    font-family: 'Microsoft YaHei', Arial, Helvetica, sans-serifsans-serif;
+    color: #222;
+
+    .i-link {
+      padding: 0 14px;
+      transition: 0.15s background-color;
+      font-size: 13px;
+    }
+
+    a {
+      color: #222;
+    }
+  }
+
+  .b-header-blur {
+    background-color: transparent;
+
+    .b-header-mask-wrp {
+      position: absolute;
+      top: 0px;
+      left: 0px;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      background-color: #fff;
+
+      .b-header-mask-bg {
+        position: absolute;
+        height: 62px;
+        padding: 0 20px;
+        top: -10px;
+        left: -20px;
+        width: 100%;
+        filter: blur(5px);
+        z-index: 50;
+        background-position: center 0 !important;
+        background-repeat: no-repeat;
+      }
+
+      .b-header-mask {
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 100%;
+        z-index: 100;
+        background-color: rgba(0, 0, 0, 0.4);
+      }
+    }
+
+    .z_header {
+      display: block;
+      margin: 0 auto;
+      position: relative;
+      text-align: left;
+      z-index: 10000;
+      width: 980px;
+      zoom: 1;
+
+      &:after {
+        content: '';
+        display: block;
+        visibility: hidden;
+        height: 0;
+        clear: both;
+        font-size: 0;
+      }
+
+      .z_top_nav {
+        float: left;
+        height: 42px;
+        font-size: 12px;
+
+        li {
+          .new {
+            position: absolute;
+            right: -8px;
+            color: #f25d8e;
+            top: 13px;
+            background: url('../../assets/images/icons.png') no-repeat;
+            width: 22px;
+            height: 10px;
+            background-position: -851px -412px;
+          }
+
+          a.i-link {
+            font-size: 12px;
+          }
+        }
+
+        ul {
+          zoom: 1;
+
+          &:after {
+            content: '';
+            display: block;
+            visibility: hidden;
+            height: 0;
+            clear: both;
+            font-size: 0;
+          }
+
+          li {
+            float: left;
+            text-align: center;
+            line-height: 42px;
+            position: relative;
+            list-style-stype: none;
+
+            a.i-link {
+              height: 100%;
+              display: block;
+              color: #fff;
+            }
+
+            &.home {
+              margin-left: -10px;
+              background-image: url('../../assets/images/icons.png');
+              background-repeat: no-repeat;
+              background-position: -845px -74px;
+
+              .i-link {
+                width: 50px;
+                height: 42px;
+                padding: 0 0 0 20px;
+              }
+            }
+
+            &.hbili {
+              position: relative;
+            }
+
+            &.live {
+              position: relative;
+            }
+          }
+        }
+      }
+
+      .uns_box {
+        float: right;
+        font-size: 12px;
+
+        ul {
+          &.menu {
+            float: left;
+            position: relative;
+          }
+
+          li {
+            list-style-type: none;
+
+            &.u-i {
+              float: left;
+              text-align: center;
+              height: 42px;
+              line-height: 42px;
+              position: relative;
+
+              a.i-link {
+                display: block;
+                color: #222;
+              }
+
+              &#i_menu_login_reg {
+                padding: 0 10px;
+
+                .i-link {
+                  display: inline-block;
+                  padding: 0 10px;
+                  color: #fff;
+
+                  span {
+                    display: block;
+                    margin: 0px;
+                    padding: 0px;
+                  }
+                }
+
+                .s-line {
+                  display: inline-block;
+                  border-left: 1px solid #fff;
+                  height: 12px;
+                  margin-top: 16px;
+                  vertical-align: top;
+                }
+              }
+
+              &.b-post {
+                margin-right: 0px;
+
+                a.i-link {
+                  margin: 0px;
+                  padding: 0px;
+                  width: 58px;
+                  position: relative;
+                  z-index: 100;
+                  background: url('../../assets/images/b-post.png') center center no-repeat;
+                  height: 48px;
+                  border-radius: 0 0 5px 5px;
+                  color: #fff !important;
+                  font-size: 14px;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
